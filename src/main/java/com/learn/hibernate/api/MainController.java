a397ae4bf225c3bd124044a7ed70cc4db8ed8593
harshpx <harsh.rzf@gmail.com>
2025-07-02 11:11:07 +0530

    removed unused imports

--- src/main/java/com/learn/hibernate/api/MainController.java
+++ src/main/java/com/learn/hibernate/api/MainController.java
@@ -3,7 +3,6 @@ package com.learn.hibernate.api;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.tomcat.util.http.parser.TE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
